713edac7e88243cd9ba7817abbb6c8a404542a47
thierry WADJI <wadjith@yahoo.fr>
2022-08-15 14:47:59 +0100

    implement path /artists in controller

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def venues():
 
       dataObj["city"] = venue.city
       dataObj["state"] = venue.state
-      dataObj["venues"] = [venueObj]
-      data = [dataObj]
+      dataObj["venues"].append(venueObj)
+      data.append(dataObj)
 
     else:
     # Loop data in order to save the venue
@@ -139,7 +139,7 @@ def venues():
 
           dataObj["city"] = venue.city
           dataObj["state"] = venue.state
-          dataObj["venues"] = [venueObj]
+          dataObj["venues"].append(venueObj)
           data.append(dataObj)
           break
 
@@ -299,16 +299,14 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = []
+  artists = Artist.query.all()
+  for artist in artists:
+    obj={}
+    obj["id"] = artist.id
+    obj["name"] = artist.name
+    data.append(obj)
+
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
